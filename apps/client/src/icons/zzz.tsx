3ed393b4e56d02e597b8f8cc9c19a720598a8fe8
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-11-11 13:17:54 +0100

    improve

--- apps/client/src/icons/zzz.tsx
+++ apps/client/src/icons/zzz.tsx
@@ -14,6 +14,7 @@ export function Zzz(props: ZzzProps) {
 			strokeLinecap="round"
 			strokeLinejoin="round"
 		>
+			<title>Hors-ligne</title>
 			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 			<path d="M4 12h6l-6 8h6" />
 			<path d="M14 4h6l-6 8h6" />
