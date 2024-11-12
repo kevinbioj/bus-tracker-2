65655da997258520a4cfcf48fb9049469003123b
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-11-12 20:07:17 +0100

    improve

--- apps/server/src/jobs/handle-vehicle-batch.ts
+++ apps/server/src/jobs/handle-vehicle-batch.ts
@@ -37,7 +37,7 @@ export async function handleVehicleBatch(store: JourneyStore, vehicleJourneys: V
 		}),
 	);
 
-	const limitRegister = pLimit(100);
+	const limitRegister = pLimit(10);
 	for (const vehicleJourney of vehicleJourneys) {
 		const timeSince = Temporal.Now.instant().since(vehicleJourney.updatedAt);
 		if (timeSince.total("minutes") >= 10) return;
