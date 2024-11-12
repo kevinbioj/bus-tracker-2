f4af54058b74e15b57d000569786b839c043a0f8
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-11-12 23:29:29 +0100

    improve

--- apps/server/src/database/database.ts
+++ apps/server/src/database/database.ts
@@ -4,11 +4,19 @@ import postgres from "postgres";
 
 import * as schema from "./schema.js";
 
-let connectionUrl = process.env.DATABASE_URL ?? "postgresql://bustracker:bustracker@localhost:5432/bustracker";
+let connectionUrl = process.env.DATABASE_URL;
+if (typeof connectionUrl === "undefined") {
+	throw new Error('Expected "DATABASE_URL" environment to be defined!');
+}
 
-if (typeof process.env.DATABASE_URL_FILE !== "undefined") {
-	const connectionUrlFile = await readFile(process.env.DATABASE_URL_FILE);
-	connectionUrl = connectionUrlFile.toString();
+if (connectionUrl.includes("{PASSWORD_FILE}")) {
+	const passwordFile = process.env.DATABASE_PASSWORD_FILE;
+	if (typeof passwordFile === "undefined") {
+		throw new Error('Connection URL refers to password file but no "DATABASE_PASSWORD_FILE" environment was defined!');
+	}
+
+	const password = (await readFile(passwordFile)).toString();
+	connectionUrl = connectionUrl.replace("{PASSWORD_FILE}", password);
 }
 
 const connection = postgres(connectionUrl);
