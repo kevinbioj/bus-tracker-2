1ade490ba54c63b928c13aeced0b7d21219dc866
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-11-15 21:21:36 +0100

    improve

--- providers/gtfs/configurations/pdl-cvl.mjs
+++ providers/gtfs/configurations/pdl-cvl.mjs
@@ -1,6 +1,47 @@
 /** @type {import('../src/model/source.ts').SourceOptions[]} */
 const sources = [
 	{
+		id: "angers",
+		staticResourceHref: "https://chouette.enroute.mobi/api/v1/datas/Irigo/gtfs.zip",
+		realtimeResourceHrefs: ["https://ara-api.enroute.mobi/irigo/gtfs/trip-updates"],
+		gtfsOptions: {
+			filterTrips: (trip) =>
+				[
+					"01",
+					"02",
+					"03",
+					"04",
+					"05",
+					"06",
+					"07",
+					"08",
+					"09",
+					"10",
+					"11",
+					"12",
+					"20",
+					"21",
+					"22",
+					"23",
+					"24",
+					"25",
+					"A",
+					"B",
+					"C",
+					"NavM",
+					"Tbus",
+				].includes(trip.route.id),
+		},
+		getNetworkRef: () => "IRIGO",
+	},
+	{
+		id: "angers-sub",
+		staticResourceHref: "https://pysae.com/api/v2/groups/irigo/gtfs/pub",
+		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/irigo/gtfs-rt"],
+		getNetworkRef: () => "IRIGO",
+		getVehicleRef: (descriptor) => descriptor?.label?.trim() || undefined,
+	},
+	{
 		id: "nantes",
 		staticResourceHref:
 			"https://data.nantesmetropole.fr/explore/dataset/244400404_tan-arrets-horaires-circuits/files/16a1a0af5946619af621baa4ad9ee662/download/",
