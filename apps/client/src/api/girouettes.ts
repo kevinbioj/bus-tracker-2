e2b1b399450abcf3f1a82a8a16dee93186a13751
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-11-18 20:39:29 +0100

    improve

--- apps/client/src/api/girouettes.ts
+++ apps/client/src/api/girouettes.ts
@@ -4,21 +4,34 @@ import { client } from "~/api/client";
 import type { DisposeableVehicleJourney } from "~/api/vehicle-journeys";
 import type { GirouetteData } from "~/components/interactive-map/girouette";
 
-export const GetJourneyGirouetteQuery = (journey: DisposeableVehicleJourney, enabled: boolean) =>
-	queryOptions({
-		enabled,
-		placeholderData: keepPreviousData,
-		refetchInterval: 30_000,
-		staleTime: 30_000,
-		queryKey: ["girouette", journey.networkId, journey.lineId, journey.direction, journey.destination],
-		queryFn: () => {
-			const params = new URLSearchParams();
-			params.append("networkId", journey.networkId.toString());
-			if (journey.lineId) params.append("lineId", journey.lineId.toString());
-			if (journey.direction) params.append("directionId", journey.direction.toString());
-			if (journey.destination) params.append("destination", journey.destination);
-			return client
-				.get(`girouettes?${params.toString()}`)
-				.then((response) => response.json<{ data: GirouetteData }[]>());
-		},
-	});
+export const GetJourneyGirouetteQuery = (
+  journey: DisposeableVehicleJourney,
+  enabled: boolean
+) => {
+  const destination = journey.destination ?? journey.calls?.at(-1)?.stopName;
+
+  return queryOptions({
+    enabled,
+    placeholderData: keepPreviousData,
+    refetchInterval: 30_000,
+    staleTime: 30_000,
+    queryKey: [
+      "girouette",
+      journey.networkId,
+      journey.lineId,
+      journey.direction,
+      destination,
+    ],
+    queryFn: () => {
+      const params = new URLSearchParams();
+      params.append("networkId", journey.networkId.toString());
+      if (journey.lineId) params.append("lineId", journey.lineId.toString());
+      if (journey.direction)
+        params.append("directionId", journey.direction.toString());
+      if (destination) params.append("destination", destination);
+      return client
+        .get(`girouettes?${params.toString()}`)
+        .then((response) => response.json<{ data: GirouetteData }[]>());
+    },
+  });
+};
