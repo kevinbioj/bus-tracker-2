7058b7e12cb1d50b91ca82628be5e22b34cbe8b9
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-11-20 22:09:25 +0100

    improve

---
+++ providers/gtfs/configurations/rhone-alpes.mjs
@@ -0,0 +1,22 @@
+/** @type {import('../src/model/source.ts').SourceOptions[]} */
+const sources = [
+	{
+		id: "grenoble",
+		staticResourceHref: "https://data.mobilites-m.fr/api/gtfs/SEM",
+		realtimeResourceHrefs: [],
+		getNetworkRef: () => "TAG",
+	},
+];
+
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	computeDelayMs: 30_000,
+	redisOptions: {
+		url: process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
+		username: process.env.REDIS_USERNAME,
+		password: process.env.REDIS_PASSWORD,
+	},
+	sources,
+};
+
+export default configuration;
