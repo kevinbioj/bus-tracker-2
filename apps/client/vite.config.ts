8e8e274321ebb8c70f63921d26de851ed53fd513
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-11-25 21:21:28 +0100

    improve

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -19,34 +19,16 @@ export default defineConfig({
 				start_url: "/",
 				icons: [
 					{
-						src: "images/touch/homescreen48.png",
-						sizes: "48x48",
-						type: "image/png",
-					},
-					{
-						src: "images/touch/homescreen72.png",
-						sizes: "72x72",
-						type: "image/png",
-					},
-					{
-						src: "images/touch/homescreen96.png",
-						sizes: "96x96",
-						type: "image/png",
-					},
-					{
-						src: "images/touch/homescreen144.png",
-						sizes: "144x144",
-						type: "image/png",
-					},
-					{
-						src: "images/touch/homescreen168.png",
-						sizes: "168x168",
-						type: "image/png",
-					},
-					{
-						src: "images/touch/homescreen192.png",
+						src: "/web-app-manifest-192x192.png",
 						sizes: "192x192",
 						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "/web-app-manifest-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
 					},
 				],
 			},
