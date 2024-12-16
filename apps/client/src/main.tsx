12017647a393e985ad0010f3a8e486f991d1d20a
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-12-16 21:05:16 +0100

    improve

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import dayjs from "dayjs";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -7,6 +8,17 @@ import "./styles/index.css";
 import "./setup-dayjs.js";
 
 import relativeTime from "dayjs/plugin/relativeTime.js";
+
+if (import.meta.env.PROD) {
+	Sentry.init({
+		dsn: "https://111a5d771b7db84170a379cea2023fa1@o4507544469176320.ingest.de.sentry.io/4508479587549264",
+		integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+		tracesSampleRate: 1.0,
+		tracePropagationTargets: [/^https:\/\/dev\.bus-tracker\.fr\/api/],
+		replaysSessionSampleRate: 0.3,
+		replaysOnErrorSampleRate: 1.0,
+	});
+}
 
 dayjs.extend(relativeTime);
 
