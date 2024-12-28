6272feb43f82a23e41965508cee72fd09f77614f
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-12-28 20:46:35 +0100

    improve

--- apps/client/src/components/ui/link.tsx
+++ apps/client/src/components/ui/link.tsx
@@ -5,7 +5,13 @@ import { Link as RouterLink } from "react-router-dom";
 type LinkProps = ComponentPropsWithRef<typeof RouterLink>;
 
 function _Link({ className, ...props }: LinkProps, ref?: ForwardedRef<HTMLAnchorElement>) {
-	return <RouterLink {...props} className={clsx("transition-colors hover:text-foreground/70", className)} ref={ref} />;
+	return (
+		<RouterLink
+			{...props}
+			className={clsx("transition-colors text-foreground/70 hover:text-foreground/50", className)}
+			ref={ref}
+		/>
+	);
 }
 
 export const Link = forwardRef(_Link);
