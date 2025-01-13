dd13bd85b77cae08cccd75e916aba1ceda698933
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-01-13 23:37:44 +0100

    tc-infos icon on vehicle details

--- apps/client/src/pages/vehicle-details.tsx
+++ apps/client/src/pages/vehicle-details.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import clsx from "clsx";
 import { Link, useParams } from "react-router-dom";
 
 import { GetNetworkQuery } from "~/api/networks";
@@ -13,7 +14,11 @@ import {
 	BreadcrumbPage,
 	BreadcrumbSeparator,
 } from "~/components/ui/breadcrumb";
+import { Button } from "~/components/ui/button";
 import { Separator } from "~/components/ui/separator";
+import tcInfosIcon from "~/icons/tc-infos.png";
+
+const getTcInfosLink = (tcId: number) => `https://tc-infos.fr/vehicule/${tcId}`;
 
 export function VehicleDetails() {
 	const { vehicleId } = useParams();
@@ -27,27 +32,36 @@ export function VehicleDetails() {
 	return (
 		<main className="p-3 max-w-screen-lg w-full mx-auto">
 			<NetworkHeader network={network} />
-			<Breadcrumb className="mt-3">
-				<BreadcrumbList>
-					<BreadcrumbItem>
-						<BreadcrumbLink asChild>
-							<Link to="/data">Données</Link>
-						</BreadcrumbLink>
-					</BreadcrumbItem>
-					<BreadcrumbSeparator />
-					<BreadcrumbItem>
-						<BreadcrumbLink asChild>
-							<Link to={`/data/networks/${network.id}`}>{network.name}</Link>
-						</BreadcrumbLink>
-					</BreadcrumbItem>
-					<BreadcrumbSeparator />
-					<BreadcrumbItem>
-						<BreadcrumbPage>
-							{vehicle.designation ?? "Véhicule"} n°{vehicle.number}
-						</BreadcrumbPage>
-					</BreadcrumbItem>
-				</BreadcrumbList>
-			</Breadcrumb>
+			<div className="relative">
+				<Breadcrumb className={clsx("mt-3", { "mr-10": typeof vehicle.tcId !== "undefined" })}>
+					<BreadcrumbList>
+						<BreadcrumbItem>
+							<BreadcrumbLink asChild>
+								<Link to="/data">Données</Link>
+							</BreadcrumbLink>
+						</BreadcrumbItem>
+						<BreadcrumbSeparator />
+						<BreadcrumbItem>
+							<BreadcrumbLink asChild>
+								<Link to={`/data/networks/${network.id}`}>{network.name}</Link>
+							</BreadcrumbLink>
+						</BreadcrumbItem>
+						<BreadcrumbSeparator />
+						<BreadcrumbItem>
+							<BreadcrumbPage>
+								{vehicle.designation ?? "Véhicule"} n°{vehicle.number}
+							</BreadcrumbPage>
+						</BreadcrumbItem>
+					</BreadcrumbList>
+				</Breadcrumb>
+				{vehicle.tcId ? (
+					<Button asChild className="absolute right-0 bottom-0" size="icon">
+						<Link target="_blank" to={getTcInfosLink(vehicle.tcId)}>
+							<img className="rounded-sm" src={tcInfosIcon} alt="Voir sur TC-Infos" />
+						</Link>
+					</Button>
+				) : null}
+			</div>
 			<Separator className="my-1" />
 			<VehicleActivities vehicleId={vehicle.id} />
 		</main>
