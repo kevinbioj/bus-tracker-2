a303f6e9d7f335fc535b0d935602fee3cd90a314
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-02-13 19:04:16 +0100

    forgot utility fn

--- providers/gtfs/configurations/occitanie.mjs
+++ providers/gtfs/configurations/occitanie.mjs
@@ -1,3 +1,14 @@
+function nthIndexOf(input, pattern, n) {
+	const length = input.length;
+	let i = -1;
+	let j = n;
+	while (j-- && i++ < length) {
+		i = input.indexOf(pattern, i);
+		if (i < 0) break;
+	}
+	return i;
+}
+
 /** @type {import('../src/model/source.ts').SourceOptions[]} */
 const sources = [
 	{
