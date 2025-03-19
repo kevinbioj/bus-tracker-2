d25cd223743e99afbea3f17f56a615b27093ffa2
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-03-19 07:39:33 +0100

    astuce: restore old excludeScheduled for tcar

--- providers/gtfs/configurations/rouen-astuce.mjs
+++ providers/gtfs/configurations/rouen-astuce.mjs
@@ -12,9 +12,9 @@ const sources = [
 			shapesStrategy: "IGNORE",
 		},
 		// 2025-03-18: run trips in scheduled mode until real-time gets fixed
-		excludeScheduled: (trip) =>
-			["ELB", "IST", "INT", "HANGA"].some((id) => trip.service.id.includes(id)) && trip.route.id !== "06",
-		// excludeScheduled: (trip) => !["06", "89", "98"].includes(trip.route.id),
+		// excludeScheduled: (trip) =>
+		// 	["ELB", "IST", "INT", "HANGA"].some((id) => trip.service.id.includes(id)) && trip.route.id !== "06",
+		excludeScheduled: (trip) => !["06", "89", "98"].includes(trip.route.id),
 		getNetworkRef: () => "ASTUCE",
 		getOperatorRef: (journey, vehicle) => {
 			if (journey?.trip.route.id === "06" || journey?.trip.route.id === "89") return "TNI";
