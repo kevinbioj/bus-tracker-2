14456699ae13888ea94b148fc9816c7a1a5423d6
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-03-29 07:33:33 +0100

    show actual elapsed seconds when 45 <= s < 60

--- apps/client/src/setup-dayjs.ts
+++ apps/client/src/setup-dayjs.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 import durationPlugin from "dayjs/plugin/duration";
 import localizedFormatPlugin from "dayjs/plugin/localizedFormat";
+import relativeTimePlugin from "dayjs/plugin/relativeTime";
 import updateLocalePlugin from "dayjs/plugin/updateLocale";
 
 import frenchLocale from "dayjs/locale/fr";
@@ -11,10 +12,29 @@ dayjs.extend(durationPlugin);
 dayjs.extend(localizedFormatPlugin);
 dayjs.extend(updateLocalePlugin);
 
+dayjs.extend(relativeTimePlugin, {
+	thresholds: [
+		{ l: "s", r: 1 },
+		{ l: "ss", r: 59, d: "second" },
+		{ l: "m", r: 1 },
+		{ l: "mm", r: 59, d: "minute" },
+		{ l: "h", r: 1 },
+		{ l: "hh", r: 23, d: "hour" },
+		{ l: "d", r: 1 },
+		{ l: "dd", r: 29, d: "day" },
+		{ l: "M", r: 1 },
+		{ l: "MM", r: 11, d: "month" },
+		{ l: "y", r: 1 },
+		{ l: "yy", d: "year" },
+	],
+	rounding: Math.floor,
+});
+
 dayjs.updateLocale("fr", {
 	relativeTime: {
 		...frenchLocale.relativeTime,
-		s: "%d secondes",
+		s: "%d seconde",
+		ss: "%d secondes",
 	},
 });
 
