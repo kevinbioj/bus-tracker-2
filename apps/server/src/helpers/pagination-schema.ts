15e006da0323a029299abddd6b06c5a57c6697d9
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-03-31 23:14:13 +0200

    server: rise up default limit to 10k

--- apps/server/src/helpers/pagination-schema.ts
+++ apps/server/src/helpers/pagination-schema.ts
@@ -4,8 +4,8 @@ export const paginationSchema = z.object({
 	limit: z.coerce
 		.number()
 		.int("Limit must be an integer")
-		.min(10, "Limit must be within [10; 100]")
-		.max(1000, "Limit must be within [10; 1000]")
-		.default(1000),
+		.min(10, "Limit must be within [10; 10000]")
+		.max(10000, "Limit must be within [10; 100000]")
+		.default(10000),
 	page: z.coerce.number().int("Page must be an integer").min(0, "Page must be a positive integer").default(0),
 });
