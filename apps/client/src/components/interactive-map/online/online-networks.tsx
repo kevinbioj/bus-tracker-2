70416753f058a71ac87ace03af6ec0aafe233df9
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-04-17 20:48:56 +0200

    update favorite icon + show favorite networks in data tab

--- apps/client/src/components/interactive-map/online/online-networks.tsx
+++ apps/client/src/components/interactive-map/online/online-networks.tsx
@@ -30,40 +30,44 @@ export function OnlineNetworks({ updateNetwork }: Readonly<OnlineNetworksProps>)
 		}
 	};
 
-	const renderNetwork = (network: Network) => (
-		<div className="h-16 relative w-full" key={network.id}>
-			<Button
-				className="absolute top-3.5 left-1"
-				onClick={() => toggleFavoriteNetwork(network.id)}
-				size="icon"
-				variant="ghost"
-			>
-				{favoriteNetworkIds.includes(network.id) ? <StarIcon fill="#FFC500" /> : <StarIcon />}
-			</Button>
-			<Button
-				className="border border-border flex justify-between items-center h-16 pr-4 pl-12 py-2 rounded-lg shadow-md transition-colors w-full bg-primary hover:bg-primary/70 text-primary-foreground"
-				onClick={() => updateNetwork(network.id)}
-				style={{
-					backgroundColor: network.color ?? undefined,
-					color: network.textColor ?? undefined,
-				}}
-			>
-				{network.logoHref ? (
-					<div className="h-full w-full">
-						<picture>
-							{network.darkModeLogoHref !== null ? (
-								<source srcSet={network.darkModeLogoHref} media="(prefers-color-scheme: dark)" />
-							) : null}
-							<img className="h-full object-contain mx-auto" src={network.logoHref} alt="" />
-						</picture>
-					</div>
-				) : (
-					<h3 className="font-bold text-center text-xl w-full">{network.name}</h3>
-				)}
-				<ArrowRight />
-			</Button>
-		</div>
-	);
+	const renderNetwork = (network: Network) => {
+		const isFavorite = favoriteNetworkIds.includes(network.id);
+
+		return (
+			<div className="h-16 relative w-full" key={network.id}>
+				<Button
+					className="absolute top-3.5 left-1"
+					onClick={() => toggleFavoriteNetwork(network.id)}
+					size="icon"
+					variant="ghost"
+				>
+					{isFavorite ? <StarIcon className="fill-yellow-400 stroke-yellow-600" /> : <StarIcon />}
+				</Button>
+				<Button
+					className="border border-border flex justify-between items-center h-16 pr-4 pl-12 py-2 rounded-lg shadow-md transition-colors w-full bg-primary hover:bg-primary/70 text-primary-foreground"
+					onClick={() => updateNetwork(network.id)}
+					style={{
+						backgroundColor: network.color ?? undefined,
+						color: network.textColor ?? undefined,
+					}}
+				>
+					{network.logoHref ? (
+						<div className="h-full w-full">
+							<picture>
+								{network.darkModeLogoHref !== null ? (
+									<source srcSet={network.darkModeLogoHref} media="(prefers-color-scheme: dark)" />
+								) : null}
+								<img className="h-full object-contain mx-auto" src={network.logoHref} alt="" />
+							</picture>
+						</div>
+					) : (
+						<h3 className="font-bold text-center text-xl w-full">{network.name}</h3>
+					)}
+					<ArrowRight />
+				</Button>
+			</div>
+		);
+	};
 
 	return (
 		<div className="flex flex-col gap-4">
--- apps/client/src/pages/network-list.tsx
+++ apps/client/src/pages/network-list.tsx
@@ -1,9 +1,10 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, StarIcon } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useLocalStorage } from "usehooks-ts";
 
-import { GetNetworksQuery } from "~/api/networks";
-import { GetRegionsQuery } from "~/api/regions";
+import { GetNetworksQuery, Network } from "~/api/networks";
+import { GetRegionsQuery, Region } from "~/api/regions";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "~/components/ui/accordion";
 import { Separator } from "~/components/ui/separator";
 import { cn } from "~/utils/utils";
@@ -11,6 +12,10 @@ import { cn } from "~/utils/utils";
 export function NetworkList() {
 	const { data: regions } = useSuspenseQuery(GetRegionsQuery);
 	const { data: networks } = useSuspenseQuery(GetNetworksQuery);
+
+	const [favoriteNetworkIds] = useLocalStorage<number[]>("favorite-networks", []);
+
+	const favoriteNetworks = networks.filter(({ id }) => favoriteNetworkIds.includes(id));
 
 	const relevantNetworksByRegion = Map.groupBy(
 		networks.filter(({ hasVehiclesFeature }) => hasVehiclesFeature),
@@ -28,9 +33,29 @@ export function NetworkList() {
 				<Separator />
 				<Accordion
 					className="mt-3"
-					defaultValue={Array.from(relevantNetworksByRegion.keys()).map((region) => region?.id.toString() ?? "-1")}
+					defaultValue={[
+						"0",
+						...Array.from(relevantNetworksByRegion.keys()).map((region) => region?.id.toString() ?? "-1"),
+					]}
 					type="multiple"
 				>
+					{favoriteNetworks.length > 0 && (
+						<NetworksAccordion
+							region={{
+								id: 0,
+								name: (
+									<>
+										<span>
+											<StarIcon className="inline align-text-bottom fill-yellow-400 stroke-yellow-600" /> Réseaux
+											favoris
+										</span>
+									</>
+								),
+								sortOrder: 0,
+							}}
+							networks={favoriteNetworks}
+						/>
+					)}
 					{Array.from(relevantNetworksByRegion.entries())
 						.toSorted(([a], [b]) => {
 							if (a === null) return 1;
@@ -38,50 +63,58 @@ export function NetworkList() {
 							return a.sortOrder - b.sortOrder;
 						})
 						.map(([region, networks]) => (
-							<AccordionItem className="border-b-0 mb-5" key={region?.id ?? -1} value={region?.id.toString() ?? "-1"}>
-								<AccordionTrigger className="font-bold text-xl">{region?.name ?? "Autres réseaux"}</AccordionTrigger>
-								<AccordionContent className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-2 gap-3 pb-0 rounded-lg">
-									{networks.map((network) => (
-										<Link
-											className={
-												"flex border justify-between items-center h-16 px-4 py-2 rounded-lg shadow-md transition-colors bg-primary hover:bg-primary/70 text-primary-foreground"
-											}
-											key={network.id}
-											title={network.authority ? `${network.name} – ${network.authority}` : network.name}
-											to={`/data/networks/${network.id}`}
-											style={{
-												backgroundColor: network.color ?? undefined,
-												color: network.textColor ?? undefined,
-											}}
-										>
-											{network.logoHref ? (
-												<>
-													<div className="h-full w-full lg:w-40">
-														<picture>
-															{network.darkModeLogoHref !== null ? (
-																<source srcSet={network.darkModeLogoHref} media="(prefers-color-scheme: dark)" />
-															) : null}
-															<img className="h-full object-contain mx-auto" src={network.logoHref} alt="" />
-														</picture>
-													</div>
-													<Separator
-														className="mx-4 bg-foreground dark:bg-foreground hidden lg:block"
-														orientation="vertical"
-													/>
-												</>
-											) : null}
-											<div className={cn("flex-col flex-1", network.logoHref ? "hidden lg:flex" : "flex")}>
-												<h3 className="font-bold text-center lg:text-start text-xl">{network.name}</h3>
-												{network.authority ? <p className="hidden lg:block">{network.authority}</p> : null}
-											</div>
-											<ArrowRight />
-										</Link>
-									))}
-								</AccordionContent>
-							</AccordionItem>
+							<NetworksAccordion key={region?.id ?? -1} region={region} networks={networks} />
 						))}
 				</Accordion>
 			</main>
 		</>
+	);
+}
+
+type NetworksAccordionProps = {
+	region: (Omit<Region, "name"> & { name: string | React.ReactNode }) | null;
+	networks: Network[];
+};
+
+function NetworksAccordion({ region, networks }: NetworksAccordionProps) {
+	return (
+		<AccordionItem className="border-b-0 mb-5" key={region?.id ?? -1} value={region?.id.toString() ?? "-1"}>
+			<AccordionTrigger className="font-bold text-xl">{region?.name ?? "Autres réseaux"}</AccordionTrigger>
+			<AccordionContent className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-2 gap-3 pb-0 rounded-lg">
+				{networks.map((network) => (
+					<Link
+						className={
+							"flex border justify-between items-center h-16 px-4 py-2 rounded-lg shadow-md transition-colors bg-primary hover:bg-primary/70 text-primary-foreground"
+						}
+						key={network.id}
+						title={network.authority ? `${network.name} – ${network.authority}` : network.name}
+						to={`/data/networks/${network.id}`}
+						style={{
+							backgroundColor: network.color ?? undefined,
+							color: network.textColor ?? undefined,
+						}}
+					>
+						{network.logoHref ? (
+							<>
+								<div className="h-full w-full lg:w-40">
+									<picture>
+										{network.darkModeLogoHref !== null ? (
+											<source srcSet={network.darkModeLogoHref} media="(prefers-color-scheme: dark)" />
+										) : null}
+										<img className="h-full object-contain mx-auto" src={network.logoHref} alt="" />
+									</picture>
+								</div>
+								<Separator className="mx-4 bg-foreground dark:bg-foreground hidden lg:block" orientation="vertical" />
+							</>
+						) : null}
+						<div className={cn("flex-col flex-1", network.logoHref ? "hidden lg:flex" : "flex")}>
+							<h3 className="font-bold text-center lg:text-start text-xl">{network.name}</h3>
+							{network.authority ? <p className="hidden lg:block">{network.authority}</p> : null}
+						</div>
+						<ArrowRight />
+					</Link>
+				))}
+			</AccordionContent>
+		</AccordionItem>
 	);
 }
