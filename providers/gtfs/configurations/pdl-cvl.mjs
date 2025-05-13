482ea7dae5ba9f3520de276bc7a983b46be94de8
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-05-13 13:58:53 +0200

    nantes-zenbus: link two vehicles

--- providers/gtfs/configurations/pdl-cvl.mjs
+++ providers/gtfs/configurations/pdl-cvl.mjs
@@ -11,11 +11,13 @@ function nthIndexOf(input, pattern, n) {
 
 const nantesZenbusVehicleIdToLabel = new Map([
 	["zenbus:Vehicle:4865174679846912:LOC", "1007"],
+	["zenbus:Vehicle:5122400943341568:LOC", "1062"],
 	["zenbus:Vehicle:5182534931447808:LOC", "1086"],
 	["zenbus:Vehicle:901640003:LOC", "1088"],
 	["zenbus:Vehicle:886860002:LOC", "1100"],
 	["zenbus:Vehicle:4895557152669696:LOC", "1104"],
 	["zenbus:Vehicle:912870002:LOC", "1105"],
+	["zenbus:Vehicle:892890034:LOC", "1109"],
 	["zenbus:Vehicle:5201965212499968:LOC", "1111"],
 	["zenbus:Vehicle:912900002:LOC", "1130"],
 ]);
