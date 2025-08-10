21d894d52439d4363a558307722de822046812de
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-08-10 11:24:28 +0200

    yelo: fix line and stop refs being cut

--- providers/gtfs/configurations/na.mjs
+++ providers/gtfs/configurations/na.mjs
@@ -68,8 +68,8 @@ const sources = [
 		mode: "NO-TU",
 		getNetworkRef: () => "YELO",
 		getVehicleRef: (vehicle) => vehicle?.id,
-		mapLineRef: (lineRef) => lineRef.slice(nthIndexOf(lineRef, ":", 2) + 1, nthIndexOf(lineRef, ":", 3)),
-		mapStopRef: (stopRef) => stopRef.slice(nthIndexOf(stopRef, ":", 3) + 1, nthIndexOf(stopRef, ":", 4)),
+		mapLineRef: (lineRef) => lineRef.slice(nthIndexOf(lineRef, ":", 2) + 1),
+		mapStopRef: (stopRef) => stopRef.slice(nthIndexOf(stopRef, ":", 3) + 1),
 	},
 	{
 		id: "na-79",
