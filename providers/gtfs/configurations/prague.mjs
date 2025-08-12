5114e2c84f5b439aeccf05600f404101fbb75f8b
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-08-12 19:44:49 +0200

    prague: test

---
+++ providers/gtfs/configurations/prague.mjs
@@ -0,0 +1,26 @@
+/** @type {import('../src/model/source.ts').SourceOptions[]} */
+const sources = [
+	{
+		id: "prague",
+		staticResourceHref: "https://data.pid.cz/PID_GTFS.zip",
+		realtimeResourceHrefs: ["https://api.golemio.cz/v2/vehiclepositions/gtfsrt/pid_feed.pb"],
+		gtfsOptions: { shapesStrategy: "IGNORE" },
+		excludeScheduled: true,
+		mode: "NO-TU",
+		getNetworkRef: () => "PID",
+		getVehicleRef: (vehicle) => vehicle?.label,
+	},
+];
+
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	computeDelayMs: 30_000,
+	redisOptions: {
+		url: process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
+		username: process.env.REDIS_USERNAME,
+		password: process.env.REDIS_PASSWORD,
+	},
+	sources,
+};
+
+export default configuration;
