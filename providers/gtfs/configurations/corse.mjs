18ad58de79c10d5fa7ce5532e3299fe87e79c9f3
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-08-15 08:56:01 +0200

    interurbain-sud: add Via Strada

--- providers/gtfs/configurations/corse.mjs
+++ providers/gtfs/configurations/corse.mjs
@@ -1,6 +1,19 @@
 /** @type {import('../src/model/source.ts').SourceOptions[]} */
 const sources = [
 	{
+		id: "interurbain-sud",
+		staticResourceHref: "https://www.data.gouv.fr/api/1/datasets/r/fe20cb23-34b8-4965-acf7-1b28bf966891",
+		realtimeResourceHrefs: [
+			"https://ctc.plateforme-2cloud.com/api/gtfsrt/2.0/tripupdates/CTC-2298-2048-0876/bin",
+			"https://ctc.plateforme-2cloud.com/api/gtfsrt/2.0/vehiclepositions/CTC-2298-2048-0876/bin",
+		],
+		gtfsOptions: { shapesStrategy: "IGNORE" },
+		excludeScheduled: true,
+		mode: "NO-TU",
+		getNetworkRef: () => "VIA-STRADA",
+		getVehicleRef: (vehicle) => vehicle?.label,
+	},
+	{
 		id: "porto-vecchio",
 		staticResourceHref: "https://pysae.com/api/v2/groups/porto-vecchio/gtfs/pub",
 		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/porto-vecchio/gtfs-rt"],
