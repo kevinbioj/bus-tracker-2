fcfee34c52e32f65b09c1169409a7723191345af
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-08-27 21:58:14 +0200

    disable Sentry from client until further notice

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from "@sentry/react";
 import dayjs from "dayjs";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -11,14 +10,15 @@ import "./styles/index.css";
 
 import relativeTime from "dayjs/plugin/relativeTime";
 
-if (import.meta.env.PROD) {
-	Sentry.init({
-		dsn: "https://111a5d771b7db84170a379cea2023fa1@o4507544469176320.ingest.de.sentry.io/4508479587549264",
-		integrations: [Sentry.browserTracingIntegration()],
-		tracesSampleRate: 0.3,
-		tracePropagationTargets: [/^https:\/\/(?:www\.)?bus-tracker\.fr\/api/],
-	});
-}
+// 2025-08-27: Disabled until pointless errors are filtered out
+// if (import.meta.env.PROD) {
+// 	Sentry.init({
+// 		dsn: "https://111a5d771b7db84170a379cea2023fa1@o4507544469176320.ingest.de.sentry.io/4508479587549264",
+// 		integrations: [Sentry.browserTracingIntegration()],
+// 		tracesSampleRate: 0.3,
+// 		tracePropagationTargets: [/^https:\/\/(?:www\.)?bus-tracker\.fr\/api/],
+// 	});
+// }
 
 dayjs.extend(relativeTime);
 
