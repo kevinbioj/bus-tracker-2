1221288b7096e486e2da551d7e3e3db99bc03f7f
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-09-26 09:20:16 +0200

    tcl: add two auxilliary feeds + 10s refresh time

--- providers/gtfs/configurations/lyon-tcl.mjs
+++ providers/gtfs/configurations/lyon-tcl.mjs
@@ -4,16 +4,34 @@ const sources = [
 		id: "tcl",
 		staticResourceHref: "https://gtfs.bus-tracker.fr/tcl.zip",
 		realtimeResourceHrefs: ["https://gtfs.bus-tracker.fr/gtfs-rt/tcl"],
-		gtfsOptions: { shapesStrategy: true },
+		gtfsOptions: { shapesStrategy: "IGNORE" },
 		excludeScheduled: true,
 		mode: "NO-TU",
 		getNetworkRef: () => "TCL",
+	},
+	{
+		id: "transports-faure-28bi",
+		staticResourceHref: "https://pysae.com/api/v2/groups/transports-faure-28bi/gtfs/pub",
+		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/transports-faure-28bi/gtfs-rt"],
+		mode: "NO-TU",
+		getNetworkRef: () => "TCL",
+		getOperatorRef: () => "FAURE-28BI",
+		getVehicleRef: (vehicle) => vehicle?.label ?? undefined,
+	},
+	{
+		id: "cars-faure-tcl",
+		staticResourceHref: "https://pysae.com/api/v2/groups/cars-faure-tcl/gtfs/pub",
+		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/cars-faure-tcl/gtfs-rt"],
+		mode: "NO-TU",
+		getNetworkRef: () => "TCL",
+		getOperatorRef: () => "FAURE-TCL",
+		getVehicleRef: (vehicle) => vehicle?.label ?? undefined,
 	},
 ];
 
 /** @type {import('../src/configuration/configuration.ts').Configuration} */
 const configuration = {
-	computeDelayMs: 30_000,
+	computeDelayMs: 10_000,
 	redisOptions: {
 		url: process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
 		username: process.env.REDIS_USERNAME,
