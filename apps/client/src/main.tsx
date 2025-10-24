2067f729205bed3ba3507677b5c79c02ad916f59
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-10-24 22:21:41 +0200

    provide build info to posthog and set defaults to 2025-05-24

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,4 +1,4 @@
-import type { PostHogConfig } from "posthog-js";
+import { posthog } from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -9,13 +9,20 @@ import "./setup-dayjs";
 import App from "./App.jsx";
 import "./styles/index.css";
 
-const posthogOptions = {
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
 	api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-} satisfies Partial<PostHogConfig>;
+	defaults: "2025-05-24",
+	respect_dnt: true,
+});
+
+posthog.register({
+	build_hash: import.meta.env.VITE_BUILD_HASH,
+	build_timestamp: import.meta.env.VITE_BUILD_TIMESTAMP,
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={posthogOptions}>
+		<PostHogProvider client={posthog}>
 			<App />
 		</PostHogProvider>
 	</StrictMode>,
