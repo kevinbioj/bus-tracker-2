9d38dbfc58596adc7f336df89ac57fb044e2272e
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-10-24 20:07:12 +0200

    fix biome, switch to vite react-compiler version since swc won't work

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,6 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -8,7 +8,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
-		react(),
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
 		tailwindcss(),
 		tsconfigPaths(),
 		VitePWA({
