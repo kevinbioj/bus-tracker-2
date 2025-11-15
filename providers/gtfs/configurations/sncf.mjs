cc885b9bac3f27fbd9299d3f6a8b8032ab99c72b
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-11-15 20:59:49 +0100

    sncf: use new gtfs-rt

--- providers/gtfs/configurations/sncf.mjs
+++ providers/gtfs/configurations/sncf.mjs
@@ -1,37 +1,17 @@
-function nthIndexOf(input, pattern, n) {
-	const length = input.length;
-	let i = -1;
-	let j = n;
-	while (j-- && i++ < length) {
-		i = input.indexOf(pattern, i);
-		if (i < 0) break;
-	}
-	return i;
-}
-
 /** @type {import('../src/model/source.ts').SourceOptions[]} */
 const sources = [
 	{
 		id: "sncf",
 		staticResourceHref: "https://gtfs.bus-tracker.fr/sncf.zip",
-		realtimeResourceHrefs: [
-			"https://proxy.transport.data.gouv.fr/resource/sncf-all-gtfs-rt-trip-updates?token=KZL1tb49w8EZODCIq8b3RpI8DKoUB6iV27Cfw_KBoWY",
-		],
+		realtimeResourceHrefs: ["https://proxy.transport.data.gouv.fr/resource/sncf-gtfs-rt-trip-updates"],
 		excludeScheduled: true,
 		gtfsOptions: {
-			mapTripId: (tripId) => tripId.slice(0, tripId.indexOf(":")),
 			ignoreBlocks: true,
 		},
 		getAheadTime: () => 10 * 60,
-		mapTripUpdate: (tripUpdate) => {
-			tripUpdate.trip.tripId = tripUpdate.trip.tripId.slice(0, tripUpdate.trip.tripId.indexOf(":"));
-			return tripUpdate;
-		},
 		getNetworkRef: () => "SNCF",
 		getVehicleRef: (_, journey) => journey?.trip.headsign,
 		getDestination: (journey) => journey?.calls.findLast((call) => call.status !== "SKIPPED")?.stop.name,
-		mapLineRef: (lineRef) => lineRef.slice(nthIndexOf(lineRef, ":", 3) + 1, lineRef.lastIndexOf(":")),
-		mapStopRef: (stopRef) => stopRef.slice(stopRef.indexOf(":") + 1),
 	},
 	{
 		id: "trenitalia",
