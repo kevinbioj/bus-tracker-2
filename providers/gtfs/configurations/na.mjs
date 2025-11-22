0272eb542ed497cdadfce289a6c19c6df22bd6be
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-11-22 14:09:13 +0100

    na: 30D-BDX use last stop name as destination

--- providers/gtfs/configurations/na.mjs
+++ providers/gtfs/configurations/na.mjs
@@ -29,6 +29,7 @@ const sources = [
 		mapLineRef: (lineRef) => lineRef.slice(nthIndexOf(lineRef, ":", 2) + 1, nthIndexOf(lineRef, ":", 3)),
 		mapStopRef: (stopRef) => stopRef.slice(nthIndexOf(stopRef, ":", 3) + 1, nthIndexOf(stopRef, ":", 4)),
 		getNetworkRef: () => "30D-BORDEAUX",
+		getDestination: (journey) => journey?.calls.findLast((call) => call.status !== "SKIPPED")?.stop.name,
 		getVehicleRef: () => undefined,
 	},
 	{
