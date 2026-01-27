dadfcdc9cd470d75731303554c39041852a129eb
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-01-27 18:03:50 +0100

    vehicles controller: higher limit to 100K

--- apps/server/src/controllers/vehicles.ts
+++ apps/server/src/controllers/vehicles.ts
@@ -25,8 +25,8 @@ const searchVehiclesSchema = z.object({
 	limit: z.coerce
 		.number()
 		.int("Limit must be an integer")
-		.min(10, "Limit must be within [10; 10000]")
-		.max(10000, "Limit must be within [10; 100000]")
+		.min(10, "Limit must be within [10; 100000]")
+		.max(100000, "Limit must be within [10; 100000]")
 		.default(10000),
 	page: z.coerce.number().int("Page must be an integer").min(0, "Page must be a positive integer").default(0),
 	networkId: z.coerce
