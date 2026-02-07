8c1e047a44c91d30ad6412aaf9d8ac8ed04dcb7b
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-02-07 11:16:06 +0100

    better psod screen

--- apps/client/src/components/ui/link.tsx
+++ apps/client/src/components/ui/link.tsx
@@ -1,11 +1,9 @@
-import clsx from "clsx";
 import { Link as RouterLink } from "react-router-dom";
+
+import { cn } from "~/utils/utils";
 
 export function Link({ className, ...props }: React.ComponentProps<typeof RouterLink>) {
 	return (
-		<RouterLink
-			{...props}
-			className={clsx("transition-colors text-foreground/70 hover:text-foreground/50", className)}
-		/>
+		<RouterLink {...props} className={cn("transition-colors text-foreground/70 hover:text-foreground/50", className)} />
 	);
 }
--- apps/client/src/psod.tsx
+++ apps/client/src/psod.tsx
@@ -1,4 +1,4 @@
-import { FrownIcon } from "lucide-react";
+import { Copy, FrownIcon } from "lucide-react";
 import { usePostHog } from "posthog-js/react";
 import { useEffect } from "react";
 import { useLocation, useRouteError } from "react-router-dom";
@@ -26,40 +26,56 @@ export function PurpleScreenOfDeath() {
 	};
 
 	return (
-		<main className="bg-branding flex flex-col items-center justify-center gap-10 h-dvh text-branding-foreground">
-			<div className="flex items-center gap-3 h-10 my-2">
+		<div className="bg-branding text-branding-foreground h-dvh">
+			<header className="h-16 p-3 flex justify-center items-center gap-3">
 				<img className="h-full" src="/logo.svg" alt="" />
-				<span className="text-center font-bold text-3xl text-white select-none hover:cursor-default">Bus Tracker</span>
-			</div>
-			<div className="flex-1">
-				<div className="flex flex-col gap-3">
-					<FrownIcon className="mx-auto size-32" />
-					<h1 className="font-bold text-2xl text-center">Oh oh...</h1>
-				</div>
-				<div className="flex flex-col gap-8 text-center text-sm">
-					<div className="flex flex-col items-center gap-1">
+				<span className="text-center font-bold text-3xl select-none hover:cursor-default">Bus Tracker</span>
+			</header>
+			<main className="px-3 mt-10 text-base">
+				<div className="max-w-3xl mx-auto">
+					<div className="flex items-center gap-2">
+						<FrownIcon className="size-12" />
+						<h1 className="font-bold text-3xl">Une erreur est survenue</h1>
+					</div>
+					<div className="flex flex-col items-start gap-2 mt-3">
 						<p>
-							Il semblerait que quelque chose se soit mal passé !<br />
-							Relancer l'application devrait régler le problème 👍
+							Les informations relatives à l'erreur ont été remontées pour analyse.
+							<br />
+							En attendant, vous pouvez essayer de recharger l'application.
 						</p>
-						<Button asChild variant="on-branding-default">
-							<a href={embeddedNetworkId ? `/embed/${embeddedNetworkId}` : "/"}>Relancer l'application</a>
+						<Button asChild className="hover:cursor-default" variant="on-branding-default">
+							<a href={embeddedNetworkId ? `/embed/${embeddedNetworkId}` : "/"}>Recharger l'application</a>
 						</Button>
 					</div>
-					<div className="flex flex-col items-center gap-1">
-						<p>Si votre problème persiste, vous pouvez tenter de réinitialiser vos réglages.</p>
+					<div className="flex flex-col items-start gap-2 mt-8">
+						<p>Si le problème persiste, vous pouvez également essayer de réinitialiser les données de l'application.</p>
 						<Button onClick={resetApp} variant="on-branding-default">
 							Réinitialiser l'app
 						</Button>
 					</div>
-					<p>
-						Les données relatives à cet événement ont été remontées pour analyse.
-						<br />
-						Si cela perdure dans le temps, contactez-nous à l'adresse{" "}
-						<Link to="mailto:contact@bus-tracker.fr">contact@bus-tracker.fr</Link>.
-					</p>
+					<div className="flex flex-col items-start gap-2 mt-8">
+						<p>
+							Enfin, vous pouvez nous contacter à l'adresse{" "}
+							<Link
+								className="text-branding-foreground/70 hover:text-branding-foreground/50"
+								to="mailto:contact@bus-tracker.fr"
+							>
+								contact@bus-tracker.fr
+							</Link>{" "}
+							avec le rapport d'erreur ci-dessous.
+						</p>
+						<div className="border border-neutral-600 rounded-lg bg-neutral-800 p-3 w-full wrap-break-word">
+							<div className="flex justify-between">
+								<span>Rapport d'erreur</span>
+								<Button onClick={() => navigator.clipboard.writeText(String(error))} size="sm" variant="ghost">
+									<Copy className="size-4" /> Copier
+								</Button>
+							</div>
+							<div className="font-mono text-neutral-300 mt-0.5">{String(error)}</div>
+						</div>
+					</div>
 				</div>
-			</div>
-		</main>
+			</main>
+		</div>
 	);
 }
