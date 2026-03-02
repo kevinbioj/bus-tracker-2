757eb6b3a49529525a1d89d11608c192de64f527
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-03-02 07:47:53 +0100

    italie: add Turin

---
+++ providers/gtfs/configurations/italie-turin.mjs
@@ -0,0 +1,29 @@
+/** @type {import('../src/model/source.ts').SourceOptions[]} */
+const sources = [
+	{
+		id: "turin",
+		staticResourceHref: "https://www.gtt.to.it/open_data/gtt_gtfs.zip",
+		realtimeResourceHrefs: [
+			"https://percorsieorari.gtt.to.it/das_gtfsrt/trip_updates.aspx",
+			"https://percorsieorari.gtt.to.it/das_gtfsrt/vehicle_position.aspx",
+		],
+		gtfsOptions: { shapesStrategy: "IGNORE" },
+		excludeScheduled: true,
+		mode: "NO-TU",
+		getNetworkRef: () => "TURIN",
+		getVehicleRef: () => undefined,
+	},
+];
+
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	computeDelayMs: 30_000,
+	redisOptions: {
+		url: process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
+		username: process.env.REDIS_USERNAME,
+		password: process.env.REDIS_PASSWORD,
+	},
+	sources,
+};
+
+export default configuration;
