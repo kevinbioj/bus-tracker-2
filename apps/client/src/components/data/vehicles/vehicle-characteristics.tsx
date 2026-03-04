cad4de5d48334b6739b617c19e99d617a2ccdcdb
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-03-04 13:25:08 +0100

    tc-infos.com -> tc-infos.fr

--- apps/client/src/components/data/vehicles/vehicle-characteristics.tsx
+++ apps/client/src/components/data/vehicles/vehicle-characteristics.tsx
@@ -16,7 +16,7 @@ import { Button } from "~/components/ui/button";
 import { BusIcon, CoachIcon, ShipIcon, TramwayIcon, TrolleybusIcon } from "~/icons/means-of-transport";
 import tcInfosIcon from "~/icons/tc-infos.png";
 
-const getTcInfosLink = (tcId: number) => `https://tc-infos.com/vehicule/${tcId}`;
+const getTcInfosLink = (tcId: number) => `https://tc-infos.fr/vehicule/${tcId}`;
 
 type VehicleCharacteristicsProps = {
 	vehicle: Vehicle;
--- apps/server/src/api/v2/vehicles.ts
+++ apps/server/src/api/v2/vehicles.ts
@@ -35,7 +35,7 @@ const vehicleEntityToVehicleDto = (
 	number: vehicle.number,
 	designation: vehicle.designation,
 	// 2026-02-25: temporary edit
-	links: { tcInfos: vehicle.tcId ? `https://tc-infos.com/vehicule/${vehicle.tcId}` : null },
+	links: { tcInfos: vehicle.tcId ? `https://tc-infos.fr/vehicule/${vehicle.tcId}` : null },
 	network: networkEntityToNetworkDto(network),
 	operator: operator !== null ? { id: operator.id, name: operator.name } : null,
 	activity,
