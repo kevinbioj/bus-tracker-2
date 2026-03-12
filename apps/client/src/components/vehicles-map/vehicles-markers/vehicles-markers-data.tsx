05e897f5b7a7b75190242e3ae0e37adb1b6018fd
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-03-12 18:33:09 +0100

    auto-zoom to match online vehicles when filtering

--- apps/client/src/components/vehicles-map/vehicles-markers/vehicles-markers-data.tsx
+++ apps/client/src/components/vehicles-map/vehicles-markers/vehicles-markers-data.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import type maplibregl from "maplibre-gl";
-import { useEffect, useMemo } from "react";
+import maplibregl from "maplibre-gl";
+import { useEffect, useMemo, useState } from "react";
 import { useDebounceValue, useLocalStorage } from "usehooks-ts";
 
-import { type CircleMarkerFeature, GeojsonCircles } from "~/adapters/maplibre-gl/geojson-circles";
+import { GeojsonCircles, type CircleMarkerFeature } from "~/adapters/maplibre-gl/geojson-circles";
+import { useMap } from "~/adapters/maplibre-gl/map";
 import { useMapBounds } from "~/adapters/maplibre-gl/use-map-bounds";
 import { GetVehicleJourneyMarkersQuery } from "~/api/vehicle-journeys";
 import { VehiclesMarkersStatusControl } from "~/components/vehicles-map/vehicles-markers/vehicles-markers-status-control";
@@ -20,13 +21,46 @@ type VehiclesMarkersDataProps = {
 };
 
 export function VehiclesMarkersData({ lineId, networkId, source }: VehiclesMarkersDataProps) {
+	const map = useMap();
 	const [previewVehicleNumber] = useLocalStorage("preview-vehicle-number", false);
 	const [bounds] = useDebounceValue(useMapBounds(), 250);
 
-	const { data, isFetching, refetch } = useQuery(GetVehicleJourneyMarkersQuery(bounds, networkId, lineId));
+	const { data, isFetching, refetch, isPlaceholderData } = useQuery(
+		GetVehicleJourneyMarkersQuery(bounds, networkId, lineId),
+	);
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: need to refetch on bounds change
 	useEffect(() => void refetch(), [bounds, refetch]);
+
+	const [lastRefocusedLineId, setLastRefocusedLineId] = useState<number | undefined>();
+	useEffect(() => {
+		if (lineId === undefined) {
+			setLastRefocusedLineId(undefined);
+			return;
+		}
+
+		if (lineId === lastRefocusedLineId) return;
+		if (isPlaceholderData || data === undefined || data.items.length === 0) return;
+
+		const validPositions = data.items.flatMap((item) => {
+			if (item.position.latitude === 0 || item.position.longitude === 0) {
+				return [];
+			}
+
+			return [[item.position.longitude, item.position.latitude] as [number, number]];
+		});
+
+		if (validPositions.length === 0) return;
+
+		const bounds = new maplibregl.LngLatBounds(validPositions[0], validPositions[0]);
+		for (const pos of validPositions) {
+			bounds.extend(pos);
+		}
+
+		map.fitBounds(bounds, { padding: 40, maxZoom: 15 });
+
+		setLastRefocusedLineId(lineId);
+	}, [lineId, data, isPlaceholderData, map, lastRefocusedLineId]);
 
 	const features = useMemo<CircleMarkerFeature[]>(
 		() =>
