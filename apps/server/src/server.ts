dc495a6a2f912ae09412aaebb1d557815ef3c309
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-04-08 23:05:10 +0200

    implement stricter cors

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -3,5 +3,15 @@ import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 
 export const hono = new Hono();
-hono.use(cors({ origin: "*" }));
 hono.use(logger());
+hono.use(
+	cors({
+		origin: [
+			"https://bus-tracker.fr",
+			"https://www.bus-tracker.fr",
+			"https://dev.bus-tracker.fr",
+			"http://localhost:3000",
+			"http://localhost:4173",
+		],
+	}),
+);
