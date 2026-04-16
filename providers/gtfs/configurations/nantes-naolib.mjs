52c23c98341bb9ebb7ef3f3637d7c6498f4dfce8
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-04-16 19:11:00 +0200

    pdl: split Nantes to nantes-naolib

---
+++ providers/gtfs/configurations/nantes-naolib.mjs
@@ -0,0 +1,168 @@
+function nthIndexOf(input, pattern, n) {
+	const length = input.length;
+	let i = -1;
+	let j = n;
+	while (j-- && i++ < length) {
+		i = input.indexOf(pattern, i);
+		if (i < 0) break;
+	}
+	return i;
+}
+
+const nantesExcludedLines = [
+	"27",
+	"28",
+	"33",
+	"40",
+	"42",
+	"47",
+	"59",
+	"60",
+	"66",
+	"67",
+	"71",
+	"75",
+	"77",
+	"78",
+	"79",
+	"80",
+	"81",
+	"87",
+	"88",
+	"89",
+	"95",
+	"96",
+	"101",
+	"102",
+	"105",
+	"107",
+	"108",
+	"109",
+	"117",
+	"118",
+	"119",
+	"128",
+	"129",
+	"131",
+	"135",
+	"138",
+	"141",
+	"149",
+	"157",
+	"158",
+	"179",
+	"E1",
+	"E4",
+	"E5",
+	"E8",
+	"N1",
+	"N2",
+	"LCE",
+	"LCN",
+	"LCO",
+	"93",
+	"91",
+];
+
+const nantesZenbusVehicleIdToLabel = new Map([
+	["zenbus:Vehicle:4885658863665152:LOC", "1006"],
+	["zenbus:Vehicle:4865174679846912:LOC", "1007"],
+	["zenbus:Vehicle:6490944494895104:LOC", "1045"],
+	["zenbus:Vehicle:911500002:LOC", "1049"],
+	["zenbus:Vehicle:5138349385842688:LOC", "1050"],
+	// ["zenbus:Vehicle:4918306319368192:LOC", "1052"], // old Zenbus tablet id
+	["zenbus:Vehicle:4890796839927808:LOC", "1052"],
+	// ["zenbus:Vehicle:4883724853313536:LOC", "1053"], // old Zenbus tablet id
+	["zenbus:Vehicle:4915944330625024:LOC", "1053"],
+	["zenbus:Vehicle:5200553242001408:LOC", "1054"],
+	["zenbus:Vehicle:5138498984083456:LOC", "1055"],
+	["zenbus:Vehicle:5072728132419584:LOC", "1056"],
+	["zenbus:Vehicle:5132913748213760:LOC", "1057"],
+	["zenbus:Vehicle:4867052192923648:LOC", "1058"],
+	// ["zenbus:Vehicle:4801682455134208:LOC", "1059"], // old Zenbus tablet id
+	["zenbus:Vehicle:4863123157680128:LOC", "1059"],
+	["zenbus:Vehicle:5132112334159872:LOC", "1060"],
+	["zenbus:Vehicle:895000002:LOC", "1061"],
+	["zenbus:Vehicle:5122400943341568:LOC", "1062"],
+	["zenbus:Vehicle:904980019:LOC", "1063"],
+	["zenbus:Vehicle:892980002:LOC", "1064"],
+	["zenbus:Vehicle:4848532587544576:LOC", "1071"],
+	["zenbus:Vehicle:880900025:LOC", "1073"],
+	["zenbus:Vehicle:897200002:LOC", "1074"],
+	["zenbus:Vehicle:905030002:LOC", "1080"],
+	["zenbus:Vehicle:884990002:LOC", "1081"],
+	["zenbus:Vehicle:883370002:LOC", "1082"],
+	["zenbus:Vehicle:4905426115100672:LOC", "1083"],
+	["zenbus:Vehicle:885170002:LOC", "1084"],
+	["zenbus:Vehicle:909310002:LOC", "1085"],
+	["zenbus:Vehicle:5182534931447808:LOC", "1086"],
+	["zenbus:Vehicle:892890033:LOC", "1087"],
+	["zenbus:Vehicle:901640003:LOC", "1088"],
+	["zenbus:Vehicle:5095547666956288:LOC", "1089"],
+	["zenbus:Vehicle:900880003:LOC", "1090"],
+	["zenbus:Vehicle:882910002:LOC", "1091"],
+	["zenbus:Vehicle:5130500127588352:LOC", "1092"],
+	["zenbus:Vehicle:4826052418338816:LOC", "1093"],
+	["zenbus:Vehicle:5088398328987648:LOC", "1094"],
+	["zenbus:Vehicle:4848286935547904:LOC", "1095"],
+	["zenbus:Vehicle:886860002:LOC", "1100"],
+	["zenbus:Vehicle:900890002:LOC", "1101"],
+	["zenbus:Vehicle:900940030:LOC", "1102"],
+	["zenbus:Vehicle:880900026:LOC", "1103"],
+	// ["zenbus:Vehicle:4895557152669696:LOC", "1104"],
+	["zenbus:Vehicle:4807134115004416:LOC", "1104"],
+	["zenbus:Vehicle:912870002:LOC", "1105"],
+	["zenbus:Vehicle:5173084644442112:LOC", "1106"],
+	["zenbus:Vehicle:6015853221904384:LOC", "1107"],
+	["zenbus:Vehicle:5193131202969600:LOC", "1108"],
+	["zenbus:Vehicle:892890034:LOC", "1109"],
+	// ["zenbus:Vehicle:4902176938786816:LOC", "1110"],
+	["zenbus:Vehicle:4808773718769664:LOC", "1110"], // since 2025-09-27
+	["zenbus:Vehicle:5201965212499968:LOC", "1111"],
+	["zenbus:Vehicle:6267566504804352:LOC", "1112"],
+	["zenbus:Vehicle:4912837504794624:LOC", "1113"],
+	["zenbus:Vehicle:912900002:LOC", "1130"],
+	["zenbus:Vehicle:5171109932564480:LOC", "1131"],
+]);
+
+/** @type {import('../src/model/source.ts').SourceOptions[]} */
+const sources = [
+	{
+		id: "nantes",
+		staticResourceHref: "https://gtfs.bus-tracker.fr/naolib.zip",
+		realtimeResourceHrefs: [
+			`https://api.okina.fr/gateway/semgtfsrt/realtime/trip-updates/NAOLIBORG?api-key=${process.env.GRAVITEE_NAOLIBORG_API_KEY}`,
+			// "https://api.staging.okina.fr/gateway/semgtfsrt/realtime/vehicle-positions/NAOLIBORG",
+		],
+		excludeScheduled: (trip) => nantesExcludedLines.includes(trip.route.name),
+		getNetworkRef: () => "NAOLIB",
+		getVehicleRef: () => undefined,
+		isValidJourney: (journey) => !nantesExcludedLines.includes(journey.line?.number),
+		mapLineRef: (lineRef) => lineRef.slice(lineRef.lastIndexOf(":") + 1),
+		mapStopRef: (stopRef) => stopRef.slice(stopRef.lastIndexOf(":") + 1),
+		mapTripRef: (tripRef) => tripRef.slice(tripRef.lastIndexOf(":") + 1),
+		mapTripUpdate: (tripUpdate) => {
+			tripUpdate.trip.tripId = `NAOLIBORG:VehicleJourney:${tripUpdate.trip.tripId.split(":").slice(2).join(":").trim()}`;
+			tripUpdate.vehicle = undefined;
+			return tripUpdate;
+		},
+	},
+	{
+		id: "nantes-zenbus",
+		staticResourceHref: "https://zenbus.net/gtfs/static/download.zip?dataset=tan",
+		realtimeResourceHrefs: ["https://zenbus.net/gtfs/rt/poll.proto?dataset=tan"],
+		mode: "NO-TU",
+		getNetworkRef: () => "NAOLIB",
+		getVehicleRef: (vehicle) => (vehicle ? nantesZenbusVehicleIdToLabel.get(vehicle.id) : undefined),
+		mapLineRef: (lineRef) => lineRef.slice(nthIndexOf(lineRef, ":", 2) + 1, nthIndexOf(lineRef, ":", 3)),
+		mapStopRef: (stopRef) => stopRef.slice(nthIndexOf(stopRef, ":", 3) + 1, nthIndexOf(stopRef, ":", 4)),
+	},
+];
+
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	computeDelayMs: 30_000,
+	sources,
+};
+
+export default configuration;
