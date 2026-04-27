e28ca3bc01a041ba058cc9e54636863268c19063
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-04-27 22:00:46 +0200

    nantes: link 1097

--- providers/gtfs/configurations/nantes.mjs
+++ providers/gtfs/configurations/nantes.mjs
@@ -105,6 +105,7 @@ const nantesZenbusVehicleIdToLabel = new Map([
 	["zenbus:Vehicle:4826052418338816:LOC", "1093"],
 	["zenbus:Vehicle:5088398328987648:LOC", "1094"],
 	["zenbus:Vehicle:4848286935547904:LOC", "1095"],
+	["zenbus:Vehicle:5993359318450176:LOC", "1097"],
 	["zenbus:Vehicle:886860002:LOC", "1100"],
 	["zenbus:Vehicle:900890002:LOC", "1101"],
 	["zenbus:Vehicle:900940030:LOC", "1102"],
