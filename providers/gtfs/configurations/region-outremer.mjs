c5260bd27a8b2d2321d265f7562a18e84ce011fa
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-06-02 13:41:05 +0200

    region-outremer: add 971 networks

--- providers/gtfs/configurations/region-outremer.mjs
+++ providers/gtfs/configurations/region-outremer.mjs
@@ -1,7 +1,43 @@
 /** @type {import('../src/model/source.ts').SourceOptions[]} */
 const sources = [
 	{
-		id: "alterneo",
+		id: "971-karulis",
+		staticResourceHref: "https://www.data.gouv.fr/api/1/datasets/r/ac89688b-252b-4b23-9e97-39226402cf2b",
+		realtimeResourceHrefs: [],
+		gtfsOptions: {
+			computeShapeDistTraveled: "always",
+			postLoad: (resource) => {
+				resource.routes.forEach((route) => {
+					route.agency.timeZone = "America/Guadeloupe";
+				});
+			},
+		},
+		getNetworkRef: () => "KARULIS",
+	},
+	{
+		id: "971-tungt",
+		staticResourceHref: "https://www.data.gouv.fr/api/1/datasets/r/dbfbff0f-46b6-44d5-a4d4-60d4ac645026",
+		realtimeResourceHrefs: [],
+		gtfsOptions: {
+			computeShapeDistTraveled: "always",
+			postLoad: (resource) => {
+				resource.routes.forEach((route) => {
+					route.agency.timeZone = "America/Guadeloupe";
+				});
+			},
+		},
+		getNetworkRef: () => "TUNGT",
+	},
+	{
+		id: "972-sud-lib",
+		staticResourceHref: "https://gtfs.bus-tracker.fr/sudlib.zip",
+		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/sud_lib/gtfs-rt"],
+		mode: "NO-TU",
+		getNetworkRef: () => "SUD-LIB",
+		getVehicleRef: (vehicle) => vehicle?.label,
+	},
+	{
+		id: "974-alterneo",
 		staticResourceHref: "https://transport.data.gouv.fr/resources/80676/download",
 		realtimeResourceHrefs: [
 			"https://proxy.transport.data.gouv.fr/resource/alterneo-civis-gtfs-rt-trip-update",
@@ -14,7 +50,7 @@ const sources = [
 			journey?.calls.findLast((call) => call.status !== "SKIPPED")?.stop.name ?? journey?.trip.headsign,
 	},
 	{
-		id: "car-jaune",
+		id: "974-car-jaune",
 		staticResourceHref: "https://pysae.com/api/v2/groups/car-jaune/gtfs/pub",
 		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/car-jaune/gtfs-rt"],
 		mode: "NO-TU",
@@ -23,26 +59,18 @@ const sources = [
 		getVehicleRef: (vehicle) => vehicle?.label,
 	},
 	{
-		id: "citalis",
+		id: "974-citalis",
 		staticResourceHref: "https://gtfs.bus-tracker.fr/citalis.zip",
 		realtimeResourceHrefs: [],
 		getNetworkRef: () => "CITALIS",
 	},
 	{
-		id: "karouest",
+		id: "974-karouest",
 		staticResourceHref: "https://pysae.com/api/v2/groups/semto-2/gtfs/pub",
 		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/semto-2/gtfs-rt"],
 		mode: "NO-TU",
 		getNetworkRef: () => "KAROUEST",
 		getDestination: (journey) => journey?.calls?.findLast((call) => call.status !== "SKIPPED")?.stop.name,
-		getVehicleRef: (vehicle) => vehicle?.label,
-	},
-	{
-		id: "sud-lib",
-		staticResourceHref: "https://gtfs.bus-tracker.fr/sudlib.zip",
-		realtimeResourceHrefs: ["https://pysae.com/api/v2/groups/sud_lib/gtfs-rt"],
-		mode: "NO-TU",
-		getNetworkRef: () => "SUD-LIB",
 		getVehicleRef: (vehicle) => vehicle?.label,
 	},
 ];
