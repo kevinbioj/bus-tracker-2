c9ce7d2e9bf6d8d1e4e6d8a9bcc90ff71b32db06
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-09-09 00:14:58 +0200

    region-normandie: compute data every 10s

--- providers/gtfs/configurations/region-normandie.mjs
+++ providers/gtfs/configurations/region-normandie.mjs
@@ -529,7 +529,7 @@ const sources = [
 /** @type {import('../src/configuration/configuration.ts').Configuration} */
 const configuration = {
 	id: "normandie",
-	computeDelayMs: 20_000,
+	computeDelayMs: 10_000,
 	sources,
 };
 
