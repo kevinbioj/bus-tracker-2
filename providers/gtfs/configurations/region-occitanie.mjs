0414b59c3bd7010a36d37f35cad7f817c7aa0925
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-09-10 20:09:04 +0200

    region-occitanie: remove montpellier and montpellier-sub sources

--- providers/gtfs/configurations/region-occitanie.mjs
+++ providers/gtfs/configurations/region-occitanie.mjs
@@ -191,26 +191,6 @@ const sources = [
 		getVehicleRef: (vehicle) => vehicle?.label,
 	},
 	{
-		id: "montpellier",
-		staticResourceHref: "https://data.montpellier3m.fr/GTFS/Urbain/GTFS.zip",
-		realtimeResourceHrefs: [
-			"https://data.montpellier3m.fr/GTFS/Urbain/VehiclePosition.pb",
-			"https://data.montpellier3m.fr/GTFS/Urbain/TripUpdate.pb",
-		],
-		mode: "NO-TU",
-		getNetworkRef: () => "TAM",
-	},
-	{
-		id: "montpellier-sub",
-		staticResourceHref: "https://data.montpellier3m.fr/GTFS/Suburbain/GTFS.zip",
-		realtimeResourceHrefs: [
-			"https://data.montpellier3m.fr/GTFS/Suburbain/VehiclePosition.pb",
-			"https://data.montpellier3m.fr/GTFS/Suburbain/TripUpdate.pb",
-		],
-		mode: "NO-TU",
-		getNetworkRef: () => "TAM",
-	},
-	{
 		id: "narbonne",
 		staticResourceHref:
 			"https://s3.eu-west-1.amazonaws.com/files.orchestra.ratpdev.com/networks/narbonne/exports/scolaires-sans-tad.zip",
