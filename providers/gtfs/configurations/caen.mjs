7171fafa076a73640b6fd3ad22ab007208c4769c
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-09-16 23:03:15 +0200

    caen: create configuration

---
+++ providers/gtfs/configurations/caen.mjs
@@ -0,0 +1,28 @@
+/** @type {import('../src/model/source.ts').SourceOptions[]} */
+const sources = [
+	{
+		id: "caen",
+		staticResourceHref:
+			"https://data.twisto.fr/api/v2/catalog/datasets/fichier-gtfs-du-reseau-twisto/alternative_exports/gtfs_twisto_zip",
+		realtimeResourceHrefs: ["https://gtfs.bus-tracker.fr/gtfs-rt/caen/"],
+		gtfsOptions: {
+			computeShapeDistTraveled: "always",
+		},
+		excludeScheduled: (trip) => {
+			const lineAsNumber = parseInt(trip.route.name, 10);
+			return Number.isNaN(lineAsNumber) || lineAsNumber < 100;
+		},
+		mode: "NO-TU",
+		getNetworkRef: () => "TWISTO",
+		getVehicleRef: (vehicle) => vehicle?.id,
+	},
+];
+
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	id: "caen",
+	computeDelayMs: 10_000,
+	sources,
+};
+
+export default configuration;
